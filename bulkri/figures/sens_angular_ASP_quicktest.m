305c6493794d0eb4390de9a856c1bb877dff92d9
Aaron Webster <aaron.webster@mpl.mpg.de>
2014-09-17 13:05:38 +0200

    sensitivities to bulkri

--- bulkri/figures/sens_angular_ASP_quicktest.m
+++ bulkri/figures/sens_angular_ASP_quicktest.m
@@ -7,7 +7,7 @@ addpath('~/mpl-dis/code/mrf_SPP_speckle')
 
 % angle range to look at
 ta = 55;
-tb = 85; 
+tb = 85;
 theta = linspace(ta,tb,10000);
 
 % fresnel r and t for both polarizations
@@ -27,30 +27,20 @@ nJ = [n1,n2,n3];
 dJ = [0,d2,0];
 kx0 = k0*n1*sin(deg2rad(theta));
 
-%% calculate angular spectrum
-parfor i=1:length(kx0)
-    [Mp, Ms] = transfer_matrix_multi(k0,kx0(i),nJ,dJ);
-    [rp1(i),~,tp1(i)] = trans_mat_2_fresnel(Mp,Ms);
+out = zeros(1,10);
+j = 1;
+nr = linspace(1.30,1.35,10);
+for j = 1:10
+    nJ = [n1,n2,nr(j)];
+    %% calculate angular spectrum
+    parfor i=1:length(kx0)
+        [Mp, Ms] = transfer_matrix_multi(k0,kx0(i),nJ,dJ);
+        [rp1(i),~,tp1(i)] = trans_mat_2_fresnel(Mp,Ms);
+    end
+    RP1 = abs(rp1).^2;
+    FR1 = fit(theta',RP1','smoothingspline');
+    out(j) = fminbnd(FR1,ta,tb);
 end
-
-nJ = [n3,n2,n1];
-parfor i=1:length(kx0)
-    [Mp, Ms] = transfer_matrix_multi(k0,kx0(i),nJ,dJ);
-    [rp2(i),~,tp2(i)] = trans_mat_2_fresnel(Mp,Ms);
-end
-
-RP1 = abs(rp1).^2;
-RP2 = abs(rp2).^2;
-TP1 = abs(tp1).^2;
-TP2 = abs(tp2).^2;
-
-%TP1 = TP1./max(TP1);
-RP2 = RP2./max(RP2);
-TP12 = TP1.*TP2;
-TP12 = TP12./max(TP12);
-
-figure(1)
-plot(theta,TP12,theta,RP1)
 
 %figure(2)
 %plot(abs((1+rp1.*rp2.*exp(2.0i.*k0.*d2)).^-4)).^2;
