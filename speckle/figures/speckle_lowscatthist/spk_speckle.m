32e0922e330b55d7b897db468e737861f75fb569
Aaron Webster <awebster@falsecolour.com>
2014-09-19 17:07:32 +0200

    add intro fig

--- speckle/figures/speckle_lowscatthist/spk_speckle.m
+++ speckle/figures/speckle_lowscatthist/spk_speckle.m
@@ -1,51 +1,50 @@
 clear all;
 close all;
-
-%% do calculation
-% number of scatterers
-M=100;
-% size of output field
-N = 1000;
-% spatial extent in real coordinates
-ext = 100;
-% k vector
-k0 = 1;
-% set up mesh
-x = linspace(-ext/2,ext/2,N);
-y = linspace(-ext/2,ext/2,N);
-[X,Y] =  meshgrid(x,y);
-% complex output field
-out = zeros(N,N);
-% random scatterers
-scatt = (rand(2,M).*ext*2-ext);
-% add them all up in a very ineffient way
-for i=1:M
-    out = out + (1./sqrt(M)).*exp((1.0i.*k0-0.00).*(sqrt((X-scatt(1,i)).^2+(Y-scatt(2,i)).^2)));
-end
-% get intensity of the fieldblue
-outa = abs(out).^2;
-ibar = mean(outa(:));
+addpath('~/mpl-dis/includes')
 
 %% fft method
 % size of initial random matrix -- input
 N = 10;
 % size of padded matrix -- output
-M = 1000;
+M = 200;
 
 % random phasors
 a = exp(2.0i*pi.*rand(N,N));
 
 % take fft, shift
-b = fft2(a,M,M);
+a = padarray(a,[M,M],0,'both');
+b = fft2(a);
 c = abs(b).^2;
 
+d = 1:size(a(:));
+d = reshape(d,size(b));
+b = b.*(-1).^d;
+b = b.*(-1).^d';
+
 % show pattern
+mycolor = brewermap(5,'Set1');
+colormap(brewermap([],'YlOrRd'))
 figure(1)
 clf;
 subplot(121)
-imagesc(c);
+%imagesc(c);
+contourf(c)
+colorbar('location','southoutside')
+set(gca,'XTickLabel','')
+set(gca,'YTickLabel','')
+set(gca,'ZTickLabel','')
 
 subplot(122);
 hold on;
-contour(real(b),[0,0],'linecolor','red');
-contour(imag(b),[0,0],'linecolor','blue');
+contour(real(b),[0,0],'linecolor',mycolor(2,:));
+contour(imag(b),[0,0],'linecolor',mycolor(1,:));
+set(gca,'XTickLabel','')
+set(gca,'YTickLabel','')
+legend('real','imag','location','southoutside','orientation','horizontal')
+
+if false
+    filename = sprintf('/tmp/test.tex');
+    matlab2tikz(filename, 'showInfo', false, ...
+        'parseStrings',false,'standalone', true, ...
+        'height', '9cm', 'width','9cm');
+end
