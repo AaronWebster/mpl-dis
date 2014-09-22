e34bfbcd74cca6e024e21c3f5ed30a2ba03c32e3
Aaron Webster <awebster@falsecolour.com>
2014-09-22 22:54:49 +0200

    add plots of sp ratio

--- scatteringmicro/figures/sp/sp.m
+++ scatteringmicro/figures/sp/sp.m
@@ -14,8 +14,6 @@ mycolor = brewermap(5,'Set1');
 x = 0:5:30;
 x_c = 0:0.1:30;
 
-
-
 theo =1.05* tan(pi*x_c/180).^2./(1+tan(pi*x_c/180).^2);
 fig = figure;
 hold on;
@@ -26,13 +24,13 @@ plot(smooth1.a(:,1),smooth1.a(:,2)./smooth1.b(:,2),'Color',mycolor(4,:))
 plot(x_c,theo,'Color',mycolor(3,:))
 
 xlabel('angle [deg]');
-ylabel('I_p/I_s [a.u.]');
+ylabel('$I_p/I_s$ [a.u.]');
 legend('measured smooth','measured rough','theoretically perfect smooth','Location','NorthWest');
 
 
-if false
-    filename = sprintf('/tmp/test.tex');
+if true
+    filename = sprintf('../spfig.tex');
     matlab2tikz(filename, 'showInfo', false, ...
-        'parseStrings',false,'standalone', true, ...
+        'parseStrings',false,'standalone', false, ...
         'height', '9cm', 'width','9cm');
 end
