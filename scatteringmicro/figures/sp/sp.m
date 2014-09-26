cf0e925cfca7e43ec552a68acd383a5daa370e16
Aaron Webster <awebster@falsecolour.com>
2014-09-26 14:03:37 +0200

    start working on surface modifications

--- scatteringmicro/figures/sp/sp.m
+++ scatteringmicro/figures/sp/sp.m
@@ -2,25 +2,27 @@ clear all;
 close all;
 addpath('~/mpl-dis/includes')
 
-
-
 % load data
 load('smooth.mat');
 load('rough.mat');
 load('smooth1.mat');
 
 mycolor = brewermap(5,'Set1');
+eps_met = LD(633e-9,'Ag','LD');
 
 x = 0:5:30;
 x_c = 0:0.1:30;
 
-theo =1.05* tan(pi*x_c/180).^2./(1+tan(pi*x_c/180).^2);
-fig = figure;
+figure(1);
+clf;
+
 hold on;
-eb_1=errorbar(x,smooth.dmean,smooth.dstd,'Color',mycolor(1,:));
+eb_1 = errorbar(x,smooth.dmean,smooth.dstd,'Color',mycolor(1,:));
 eb_2 = errorbar(x,rough.dmean,rough.dstd,'Color',mycolor(2,:));
 
 plot(smooth1.a(:,1),smooth1.a(:,2)./smooth1.b(:,2),'Color',mycolor(4,:))
+
+theo =tan(deg2rad(x_c)).^2./(1+tan(deg2rad(x_c)).^2./abs(eps_met));
 plot(x_c,theo,'Color',mycolor(3,:))
 
 xlabel('angle [deg]');
