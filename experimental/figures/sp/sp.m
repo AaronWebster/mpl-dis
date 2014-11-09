fcc0bfc1d3c85b97fa3b6abe9a892eec04a129ed
Aaron Webster <aaron.webster@mpl.mpg.de>
2014-11-09 14:18:39 +0100

    incorporate mrf suggestions to experimental section

--- experimental/figures/sp/sp.m
+++ experimental/figures/sp/sp.m
@@ -17,20 +17,20 @@ figure(1);
 clf;
 
 hold on;
-eb_1 = errorbar(x,smooth.dmean,smooth.dstd,'Color',mycolor(1,:));
 eb_2 = errorbar(x,rough.dmean,rough.dstd,'Color',mycolor(2,:));
+eb_1 = errorbar(x,smooth.dmean,smooth.dstd,'Color',mycolor(1,:));
 
-plot(smooth1.a(:,1),smooth1.a(:,2)./smooth1.b(:,2),'Color',mycolor(4,:))
+%plot(smooth1.a(:,1),smooth1.a(:,2)./smooth1.b(:,2),'Color',mycolor(4,:))
 
 theo =tan(deg2rad(x_c)).^2./(1+tan(deg2rad(x_c)).^2./abs(eps_met));
 plot(x_c,theo,'Color',mycolor(3,:))
 
 xlabel('angle [deg]');
 ylabel('$I_p/I_s$ [a.u.]');
-legend('measured smooth','measured rough','theoretically perfect smooth','Location','NorthWest');
+legend('measured rough','measured smooth','theoretical perfect smoothness','Location','NorthWest');
 
 
-if false
+if true
     filename = sprintf('../spfig.tex');
     matlab2tikz(filename, 'showInfo', false, ...
         'parseStrings',false,'standalone', false, ...
